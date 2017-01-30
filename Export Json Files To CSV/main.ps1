4cb2f394d53cfc593526ef33410501ebdc5ee683
Petter Bomban <petterbomban@gmail.com>
2017-01-30 13:05:34 +0100

    push

---
+++ Export Json Files To CSV/main.ps1
@@ -0,0 +1,16 @@
+﻿$Path = "C:\Users\admin\Documents\GitHub\Legge til vlan pa vHoster\Json"
+$ExportPath = "C:\Users\admin\Documents\GitHub\Legge til vlan pa vHoster\Json\ALL.csv"
+
+$Content = Get-ChildItem -Path $Path
+
+$Collection = @()
+$Content | foreach {
+    $JsonPath = $_.FullName
+    $Json = Get-Content -Path $JsonPath -raw
+    $Obj = $Json | ConvertFrom-Json
+    $Collection += $Obj
+}
+
+$Collection | Export-Csv -Path $ExportPath `
+                -NoTypeInformation -Force -Delimiter ";"
+$Collection

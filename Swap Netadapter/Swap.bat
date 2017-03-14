609f25d86f671c9baefc106444491eebfa89dede
Petter Bomban <petterbomban@gmail.com>
2017-03-14 12:05:00 +0100

    Swap Netadapter

---
+++ Swap Netadapter/Swap.bat
@@ -0,0 +1,3 @@
+@Echo off
+
+Powershell.exe .\SwapNetadapter.ps1
---
+++ Swap Netadapter/SwapNetadapter.ps1
@@ -0,0 +1,16 @@
+﻿if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+
+$Nics = @("Akershus-FK", "IKT-Fag")
+$Adapter = Get-NetAdapter 
+
+foreach ($NIC in $Nics)
+{
+    $CurrAdapter = $Adapter | Where-Object Name -eq $NIC
+    if ($CurrAdapter.Status -eq "Disabled") { Enable-NetAdapter -Name $CurrAdapter.Name -Confirm:$False }
+    elseif ($CurrAdapter.Status -eq "Up") { Disable-NetAdapter -Name $CurrAdapter.Name -Confirm:$False }   
+}

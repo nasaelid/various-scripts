1cc0f11f0a841220cd417649f6f908bb0351bb26
Petter Bomban <petterbomban@gmail.com>
2017-05-19 21:05:43 +0200

    Fixed bug
    Some times an adapter would not get enabled

--- Swap Netadapter/SwapNetadapter.ps1
+++ Swap Netadapter/SwapNetadapter.ps1
@@ -11,6 +11,15 @@ $Adapter = Get-NetAdapter
 foreach ($NIC in $Nics)
 {
     $CurrAdapter = $Adapter | Where-Object Name -eq $NIC
-    if ($CurrAdapter.Status -eq "Disabled") { Enable-NetAdapter -Name $CurrAdapter.Name -Confirm:$False }
-    elseif ($CurrAdapter.Status -eq "Up") { Disable-NetAdapter -Name $CurrAdapter.Name -Confirm:$False }   
+    if ($CurrAdapter.Status -eq "Disabled")
+    {
+        Enable-NetAdapter -Name $CurrAdapter.Name -Confirm:$False
+        Start-Sleep -Seconds 1
+        continue
+    }
+    if ($CurrAdapter.Status -eq "Up")
+    {
+        Disable-NetAdapter -Name $CurrAdapter.Name -Confirm:$False
+        Start-Sleep -Seconds 1
+    }   
 }
